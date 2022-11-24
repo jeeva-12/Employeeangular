6bb9e54184283a9a9c8fb75f9eaca956efffe23f
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-11-24 17:17:57 +0530

    gfd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +9,32 @@ import { ViewEmployeeComponent } from './view-employee/view-employee.component';
 import { SearchEmployeeComponent } from './search-employee/search-employee.component';
 import { DeleteEmployeeComponent } from './delete-employee/delete-employee.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AdminLoginComponent
+  },
+  {
+  path:"home",
+  component:AddEmployeeComponent
+  },
+  {
+    path:"view",
+    component:ViewEmployeeComponent
+  },
+  {
+    path:"search",
+    component:SearchEmployeeComponent
+  },
+  {path:"delete",
+  component:DeleteEmployeeComponent
+},
+
+
+]
+
 
 @NgModule({
   declarations: [
@@ -22,7 +48,9 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
